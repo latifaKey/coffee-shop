8281a868cdd2f7d0ecda54c6611543a4176a20d2
latifaKey <181171537+latifaKey@users.noreply.github.com>
2026-02-06 03:48:16 +0700

    up

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -14,12 +14,7 @@ export const prisma =
   new PrismaClient({
     log: process.env.NODE_ENV === "development" 
       ? ["warn", "error"]
-      : ["error"],
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL
-      }
-    }
+      : ["error"]
   });
 
 if (process.env.NODE_ENV !== "production") {
